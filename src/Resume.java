832faf05a9ffaaf24947e9d559919a6d84cfad85
Дмитрий Алёшин <punikson@kotbeznosa.local>
2019-03-27 19:28:53 +0300

    dobavil class

---
+++ src/Resume.java
@@ -0,0 +1,10 @@
+public class Resume {
+    int ID;
+    String firstName;
+    String lastName;
+    public Resume(int id,String firstName, String lastName){
+        this.ID = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
