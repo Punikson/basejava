40f1ee0f685b8da407404c2d87fe6ff898379fbb
Дмитрий Алёшин <punikson@kotbeznosa.local>
2019-05-05 15:57:17 +0300

    HW4 add tests and class for reflections

---
+++ src/ru/punikson/base/storage/MainReflection.java
@@ -0,0 +1,20 @@
+package ru.punikson.base.storage;
+
+import ru.punikson.base.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        field.get(r);
+        System.out.println(r);
+        Method method = r.getClass().getDeclaredMethod("toString");
+        System.out.println(method.invoke(r));
+    }
+}
---
+++ test/ru/punikson/base/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,36 @@
+package ru.punikson.base.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractArrayStorageTest {
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
